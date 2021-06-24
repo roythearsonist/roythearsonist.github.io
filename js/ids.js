a09fd5b8da996ec376e4e3f91a9ca28a123b52cd
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-06-24 01:16:20 -0400

    Revert "A lot of function stuff"
    This reverts commit e24a4d35aed60708d8098f7ab71991597a6d0a5d.

--- js/ids.js
+++ js/ids.js
@@ -1,23 +1,9 @@
-function getids(id) {
-  let element = document.getElementById(id);
+function getids() {
+  var projects = document.getElementById("projects");
 }
 function projectsid() {
-  getids(projects);
-  element.style.textAlign = "left";
-  element.style.flexDirection = "column";
-  element.style.padding = "10px";
+  getids();
+  projects.style.textAlign = "left";
+  projects.style.flexDirection = "column";
 }
-function space1() {
-  getids(space1);
-  element.style.padding = "10px";
-}
-function space2() {
-  getids(space2);
-  element.style.padding = "10px";
-}
-function useallfunctions() {
 projectsid();
-space1();
-space2();
-}
-useallfunctions();
