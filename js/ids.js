e24a4d35aed60708d8098f7ab71991597a6d0a5d
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-06-24 00:59:47 -0400

    A lot of function stuff

--- js/ids.js
+++ js/ids.js
@@ -1,9 +1,23 @@
-function getids() {
-  var projects = document.getElementById("projects");
+function getids(id) {
+  let element = document.getElementById(id);
 }
 function projectsid() {
-  getids();
-  projects.style.textAlign = "left";
-  projects.style.flexDirection = "column";
+  getids(projects);
+  element.style.textAlign = "left";
+  element.style.flexDirection = "column";
+  element.style.padding = "10px";
 }
+function space1() {
+  getids(space1);
+  element.style.padding = "10px";
+}
+function space2() {
+  getids(space2);
+  element.style.padding = "10px";
+}
+function useallfunctions() {
 projectsid();
+space1();
+space2();
+}
+useallfunctions();
