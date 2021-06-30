d5da6d228c9c301337ca067f59949ebb69a5a550
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-06-30 14:27:53 -0400

    Made the variable local instead of a function

--- js/ids.js
+++ js/ids.js
@@ -1,10 +1,7 @@
-function getids(id) {
-  let element = document.getElementById(id);
-}
 function projectsid() {
-  getids("projects");
+  let element = document.getElementById("projects");
   element.style.textAlign = "left";
-  elemeny.style.flexDirection = "column";
+  element.style.flexDirection = "column";
 }
 function useallfunctions() {
 projectsid();
