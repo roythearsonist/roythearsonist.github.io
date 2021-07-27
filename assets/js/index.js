ec5d7395d1cb1c36758b0526e29110edda286ab2
The Blob SCP <81673375+theblobscp@users.noreply.github.com>
2021-07-27 13:32:56 -0400

    Convert stuff to js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-"use strict";
+const credits = {
+  owner: {
+    link: "https://github.com/theblobscp",
+    name: "theblobscp",
+  },
+  "past-contrib": {
+    link: "https://github.com/CharlieS1103",
+    name: "CharlieS1103",
+  },
+};
